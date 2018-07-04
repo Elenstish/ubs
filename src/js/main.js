fa8a734702b98f2bd40d082db468bb38ad40bfbb
Elenstish <elenstish@gmail.com>
2018-07-05 00:12:28 +0300

    bud_service only screens

--- src/js/main.js
+++ src/js/main.js
@@ -45,43 +45,43 @@ $(document).ready(function() {
         ]
     });
 
-    // --------- scroll up
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('#scroller').fadeIn();
-        } else {
-            $('#scroller').fadeOut();
-        }
-    });
-    $('#scroller').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 900);
-        return false;
-    });
-
-  // ANIMATION
-	var doAnimations = function() {
-
-		var offset = $(window).scrollTop() + $(window).height(),
-			$animatables = $('.animatable');
-
-		if ($animatables.size() == 0) {
-			$(window).off('scroll', doAnimations);
-		}
-
-		$animatables.each(function(i) {
-			var $animatable = $(this);
-			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
-				$animatable.removeClass('animatable').addClass('animated');
-			}
-		});
-
-	};
-
-	$(window).on('scroll', doAnimations);
-	$(window).trigger('scroll');
-
+//     // --------- scroll up
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 0) {
+//             $('#scroller').fadeIn();
+//         } else {
+//             $('#scroller').fadeOut();
+//         }
+//     });
+//     $('#scroller').click(function () {
+//         $('body,html').animate({
+//             scrollTop: 0
+//         }, 900);
+//         return false;
+//     });
+//
+//   // ANIMATION
+// 	var doAnimations = function() {
+//
+// 		var offset = $(window).scrollTop() + $(window).height(),
+// 			$animatables = $('.animatable');
+//
+// 		if ($animatables.size() == 0) {
+// 			$(window).off('scroll', doAnimations);
+// 		}
+//
+// 		$animatables.each(function(i) {
+// 			var $animatable = $(this);
+// 			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+// 				$animatable.removeClass('animatable').addClass('animated');
+// 			}
+// 		});
+//
+// 	};
+//
+// 	$(window).on('scroll', doAnimations);
+// 	$(window).trigger('scroll');
+//
 });
 
 // ----------- MAP
