491877029a19e6be74a6713d0da921ccd2629938
Elenstish <elenstish@gmail.com>
2018-07-10 21:34:34 +0300

    without navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 // ----------- MAP
-
 var markersData = [
     {
         lat: 47.785054,     // Широта
         lng: 35.212481,    // Долгота
         name: "УютБудСервис", // Произвольное название, которое будем выводить в информационном окне
-        // address:"Новинский бульвар, с1121069"   // Адрес, который также будем выводить в информационном окне
+        address:"г. Запорожье, ул. Паромонова, 4"   // Адрес, который также будем выводить в информационном окне
     }
 ];
 
@@ -14,178 +13,9 @@ function initMap() {
     var centerLatLng = new google.maps.LatLng(47.785054, 35.212481);
     var mapOptions = {
         center: centerLatLng,
-        zoom: 16,
-        styles: [
-            {
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#f5f5f5"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#616161"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "color": "#f5f5f5"
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative.land_parcel",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#bdbdbd"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#eeeeee"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#757575"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#e5e5e5"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#deb33e"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.park",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#9e9e9e"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.arterial",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#757575"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#dadada"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#616161"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.local",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#9e9e9e"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit.line",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#e5e5e5"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit.station",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#eeeeee"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "color": "#c9c9c9"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#9e9e9e"
-                    }
-                ]
-            }
-        ],
+        zoom: 15
     };
-    map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
     infoWindow = new google.maps.InfoWindow();
     google.maps.event.addListener(map, "click", function() {
         infoWindow.close();
@@ -243,7 +73,7 @@ $(document).ready(function() {
 // SLIDER
     $('.responsive').slick({
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         arrows: true,
         dots: false,
@@ -251,7 +81,16 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 1240,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: false,
+                    initialSlide: 0,
+                }
+            },
+            {
+                breakpoint: 769,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
@@ -260,7 +99,7 @@ $(document).ready(function() {
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 576,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
@@ -286,27 +125,42 @@ $(document).ready(function() {
         return false;
     });
 
-//   // ANIMATION
-// 	var doAnimations = function() {
-//
-// 		var offset = $(window).scrollTop() + $(window).height(),
-// 			$animatables = $('.animatable');
-//
-// 		if ($animatables.size() == 0) {
-// 			$(window).off('scroll', doAnimations);
-// 		}
-//
-// 		$animatables.each(function(i) {
-// 			var $animatable = $(this);
-// 			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
-// 				$animatable.removeClass('animatable').addClass('animated');
-// 			}
-// 		});
-//
-// 	};
-//
-// 	$(window).on('scroll', doAnimations);
-// 	$(window).trigger('scroll');
-//
+    //CALL
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if(height > 50){
+            $('.js-callback')
+                .removeClass('.callback_none')
+                .addClass('callback');
+        } else{
+            $('.js-callback')
+                .removeClass('callback')
+                .addClass('callback_none');
+        }
+
+    });
+
+  // ANIMATION
+	var doAnimations = function() {
+
+		var offset = $(window).scrollTop() + $(window).height(),
+			$animatables = $('.animatable');
+
+		if ($animatables.size() == 0) {
+			$(window).off('scroll', doAnimations);
+		}
+
+		$animatables.each(function(i) {
+			var $animatable = $(this);
+			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+				$animatable.removeClass('animatable').addClass('animated');
+			}
+		});
+
+	};
+
+	$(window).on('scroll', doAnimations);
+	$(window).trigger('scroll');
+
 });
 
