5dfa190c0cde611484830b47a940e50f640bff3a
Elenstish <elenstish@gmail.com>
2018-07-16 13:50:34 +0300

    massonry

--- src/js/main.js
+++ src/js/main.js
@@ -211,6 +211,86 @@ $(document).ready(function() {
 	$(window).on('scroll', doAnimations);
 	$(window).trigger('scroll');
 
+
+    //***ISOTOPE***
+
+    // $('.grid').isotope({
+    //     // filter: '.okno',
+    //     itemSelector: '.grid-item',
+    //     masonry: {
+    //         columnWidth: 200,
+    //         gutter: 20
+    //     },
+    // });
+    var $grid = $('.grid').isotope({
+        // main isotope options
+        itemSelector: '.grid-item',
+        // set layoutMode
+        layoutMode: 'fitRows',
+        // options for cellsByRow layout mode
+        cellsByRow: {
+                    columnWidth: 200,
+                    rowHeight: 200
+                },
+                // options for masonry layout mode
+                masonry: {
+                    columnWidth: '.grid-sizer'
+                }
+    })
+
+    // var $grid = $('.grid').isotope({
+    //     // main isotope options
+    //     itemSelector: '.grid-item',
+    //     // set layoutMode
+    //     layoutMode: 'cellsByRow',
+    //     // options for cellsByRow layout mode
+    //     cellsByRow: {
+    //         columnWidth: 200,
+    //         rowHeight: 150
+    //     },
+    //     // options for masonry layout mode
+    //     masonry: {
+    //         columnWidth: '.grid-sizer'
+    //     }
+    // })
+
+// // init Isotope
+//     var $grid = $('.grid').isotope({
+//         itemSelector: '.grid-item',
+//         layoutMode: 'masonry'
+//     });
+
+    var filterFns = {
+        // show if number is greater than 50
+        numberGreaterThan50: function () {
+            var number = $(this).find('.number').text();
+            return parseInt(number, 10) > 50;
+        }
+    };
+// bind filter on select change
+    $('.filters-select').on( 'change', function() {
+        // get filter value from option value
+        var filterValue = this.value;
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+
+// // filter items on button click
+//     $('.filter-button-group').on( 'click', 'button', function() {
+//         var filterValue = $(this).attr('data-filter');
+//         $grid.isotope({ filter: filterValue });
+//     });
+//
+// // change is-checked class on buttons
+//     $('.btn-group').each( function( i, buttonGroup ) {
+//         var $buttonGroup = $( buttonGroup );
+//         $buttonGroup.on( 'click', 'button', function() {
+//             $buttonGroup.find('.is-checked').removeClass('is-checked');
+//             $( this ).addClass('is-checked');
+//         });
+//     });
+
 });
 
 // if(window.innerWidth < 800) {
